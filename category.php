a787ea5cbfff37ec3d078b6a6ac8ae963ca7cf06
matysBV <e2185476@cmaisonneuve.qc.ca>
2025-02-26 16:32:03 -0500

    titre categorie + description

--- category.php
+++ category.php
@@ -3,6 +3,8 @@
 <!--  -->
 <!-- <h1>------------------ CATEGORY.PHP ------------------</h1> -->
 <!--  -->
+<h1><?php single_cat_title(); ?></h1>
+<p><?php echo category_description(); ?></p>
 <section class="populaire">
     <div class="global">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
--- gabarit/carte.php
+++ gabarit/carte.php
@@ -15,7 +15,7 @@
             the_post_thumbnail('thumbnail');
         }
         ?>
-        <h2 class="carte__titre"><?php the_title(); ?></h2>
+        <h4 class="carte__titre"><?php the_title(); ?></h4>
         <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 10, "..."); ?></p>
         <a class="carte__bouton carte__bouton-actif" href="<?php the_permalink(); ?>">Suite ...</a>
         <!-- <button class="carte__bouton carte__bouton--actif">Suite</button> -->
