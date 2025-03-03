e54684a8c3d31e57ab9d35a78fb7a8f9c82b0941
matysBV <e2185476@cmaisonneuve.qc.ca>
2025-03-03 15:07:41 -0500

    post modifie / temperature / titre site

--- gabarit/carte.php
+++ gabarit/carte.php
@@ -18,6 +18,10 @@
         <h4 class="carte__titre"><?php the_title(); ?></h4>
         <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 10, "..."); ?></p>
         <a class="carte__bouton carte__bouton-actif" href="<?php the_permalink(); ?>">Suite ...</a>
+        <?php the_category(); ?>
+        <p>Température maximum : <?php the_field('temperature_maximum'); ?> °C</p>
+        <p>Température minimum : <?php the_field('temperature_minimum') ?>°C</p>
+        <p>Température moyenne : <?php the_field('temperature_moyenne') ?>°C</p>
         <!-- <button class="carte__bouton carte__bouton--actif">Suite</button> -->
     </div>
 </article>
