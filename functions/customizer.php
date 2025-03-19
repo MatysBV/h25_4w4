8e0df97e0daf55a0512c5bbb8dbd10a9b9d65e3b
MatysBV <150718597+MatysBV@users.noreply.github.com>
2025-03-18 21:17:54 -0400

    commentation des fichiers scss.

--- functions/customizer.php
+++ functions/customizer.php
@@ -10,7 +10,7 @@ function theme_tp_customize_register($wp_customize) {
 
 
     ###################### AUTEUR ######################
-    ////////////////////////// Ajout des données
+    ////////////////////////// Ajout des données  (auteur)
     $wp_customize->add_setting('hero_auteur', array(
         'default' => __('Matys Voisin', 'theme_tp'),
         'sanitize_callback' => 'sanitize_text_field'
@@ -25,13 +25,13 @@ function theme_tp_customize_register($wp_customize) {
 
 
     ################## IMAGE BACKGROUND ##################
-    // Ajout donnee
+    // Ajout donnee  (changement background)
     $wp_customize->add_setting('hero_background', array(
         'default' => '',
         'sanitize_callback' => 'esc_url_raw',
     ));
 
-    // Controle donnee
+    // Controle donnee  (image background)
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
         'label' => __('Image background', 'theme_tp'),
         'section' => 'hero_section',
@@ -48,42 +48,42 @@ function theme_tp_customize_register($wp_customize) {
         'default' => __('5800 Sherbrooke-est - Montréal (Québec) H1X 2A2', 'theme_tp'),
         'sanitize_callback' => 'sanitize_text_field'
     ));
-    ///////////////////// ajout du contrôle de la donnée
+    ///////////////////// ajout du contrôle de la donnée (adresse)
     $wp_customize->add_control('footer_adresse', array(
         'label' => __('Adresse', 'theme_tp'),
         'section' => 'footer_section',
         'type' => 'text',
     ));  
-    //////////////////////  ajout de la donnée
+    //////////////////////  ajout de la donnée (adresse)
     $wp_customize->add_setting('footer_telephone', array(
         'default' => __('514-123-1234', 'theme_tp'),
         'sanitize_callback' => 'sanitize_text_field'
     ));
-    ///////////////////// ajout du contrôle de la donnée
+    ///////////////////// ajout du contrôle de la donnée (telephone)
     $wp_customize->add_control('footer_telephone', array(
             'label' => __('Téléphone', 'theme_tp'),
             'section' => 'footer_section',
             'type' => 'text',
     ));  
-        //////////////////////  ajout de la donnée
+        //////////////////////  ajout de la donnée (telephone)
         $wp_customize->add_setting('footer_mission', array(
             'default' => __('Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, nostrum sint deserunt architecto dolorem nisi delectus pariatur odit eius! Maiores dolore provident soluta culpa minus nesciunt doloremque vero incidunt accusamus!', 'theme_tp'),
             'sanitize_callback' => 'sanitize_text_field'
         ));
-        ///////////////////// ajout du contrôle de la donnée
+        ///////////////////// ajout du contrôle de la donnée (mission)
         $wp_customize->add_control('footer_mission', array(
             'label' => __('Mission', 'theme_tp'),
             'section' => 'footer_section',
             'type' => 'text',
         ));
 
-         ///////////////////// ajout du contrôle des couleurs de texte dans le he
+         ///////////////////// ajout du contrôle des couleurs de texte dans le hero
         $wp_customize->add_setting('hero_couleur', array(
             'default' => '',
             'sanitize_callback' => 'esc_url_raw',
         ));
     
-        // Controle donnee
+        // Controle donnee  (changement de couleur dans le hero)
         $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'hero_couleur', array(
             'label' => __('sélection de la couleur', 'theme_tp'),
             'section' => 'hero_section',
