8c0083a21588d8708773d344c25ae5da392303f7
Matys Voisin <matysvoisin@gmail.com>
2025-03-24 15:31:04 -0400

    modification html (ajout d'un bouton pour report un bug

--- 404.php
+++ 404.php
@@ -1,12 +1,14 @@
-
 <div class="img-Error404-Background">
     <img src="" alt="" srcset="">
 </div>
 
-<div class="error404"> 
+<div class="error404">
     <h1>404</h1>
     <p> OOPS! PAGE NOT FOUND</p>
     <?php get_search_form(); ?>
     <div class="error404__icone"><?php get_template_part('gabarit/icones'); ?></div>
-    <p><a href="<?php echo get_home_url(); ?>"  class="error404__menu">Retourner à l'accueil</a></p>
+    <p><a href="<?php echo get_home_url(); ?>" class="error404__menu">Retourner à l'accueil</a></p>
+    <div class="error404__reportBug">
+        <button class="error404__bug">Report a bug</button>
+    </div>
 </div>
