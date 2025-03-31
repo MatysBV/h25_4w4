553eb0608aa0f8cd74e68bc541e8a6b5793d654f
Matys Voisin <matysvoisin@gmail.com>
2025-03-31 15:31:58 -0400

    css version pc complété

--- 404.php
+++ 404.php
@@ -6,14 +6,21 @@
 
 
 <section class="section_404" style=" background-image: url(<?php echo $section_404_background ?>); color : <?php echo $section_404_couleur ?>">
-    <div class="">
+    <div class="section_404_texte">
         <?php get_template_part('gabarit/error404'); ?>
 
-        <p><a href="<?php echo get_home_url(); ?>" class="error404__menu">Retourner à l'accueil</a></p>
+        <p><a href="<?php echo get_home_url(); ?>" class="error404__RetourMenu">Retourner à l'accueil</a></p>
+    </div>
 
-        <div class="section_404__icone">
-            <?php get_template_part('gabarit/icones'); ?>
-        </div>
+    <div class="menu-404error">
+        <?php wp_nav_menu(array(
+            "menu" => "404 error",
+            "container" => "nav",
+        )); ?>
+    </div>
+
+    <div class="section_404__icone">
+        <?php get_template_part('gabarit/icones'); ?>
     </div>
 
 </section>
