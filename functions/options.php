6d5f0525ca1730a3dad4d257388f4ec96b80eecd
Matys Voisin <e2185476@cmaisonneuve.qc.ca>
2025-04-02 17:29:59 -0400

    s10c2 : ajout function genere bouton

--- functions/options.php
+++ functions/options.php
@@ -15,7 +15,7 @@ function mon_theme_supports() {
 add_action( 'after_setup_theme', 'mon_theme_supports' );
 
 function theme_4w4_enqueue_styles(){
-    wp_enqueue_style('mon-style-style', get_template_directory_uri() . '/normalize.css');
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/normalize.css');
     wp_enqueue_style('mon-style-style', get_stylesheet_uri());
 
 
