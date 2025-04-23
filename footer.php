0fde9478474df7f530b351baaed82ee6d8a66479
MatysBV <matysvoisin@gmail.com>
2025-04-23 16:59:46 -0400

    s12c2: ajout du customizer pour changer la couleur du footer et de la vague

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-<?php genere_vague(); ?>
-
-<footer>
-
 <?php $footer_adresse = get_theme_mod('footer_adresse', '5800 Sherbrooke-est Montréal (Québec) H1X 2A2'); ?>
 <?php $footer_telephone = get_theme_mod('footer_telephone', '(514-254-7131)'); ?>
 <?php $footer_mission = get_theme_mod('footer_mission', 'Notre mission est de faciliter vos recherche afin de découvrir les plus beaux endroit de la planète'); ?>
+<?php $footer_couleur = get_theme_mod('footer_couleur', '#aaa'); ?>
 
+<?php genere_vague($footer_couleur); ?>
+
+<footer style="background-color: <?= $footer_couleur ?>">
 
     <div class="piedpage">
         <section class="piedpage__s1">
